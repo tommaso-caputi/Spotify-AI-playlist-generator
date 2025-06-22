14020e2e491760d565bc5cc6f8b2febfd7ec3795
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2025-06-22 13:39:15 +0200

    clean code

--- spotify-playlist-generator/src/app/dashboard/page.tsx
+++ spotify-playlist-generator/src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Playlists from '../components/Playlists';
+import Playlists from '../../components/Playlists';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
--- spotify-playlist-generator/src/components/Playlists.tsx
+++ spotify-playlist-generator/src/components/Playlists.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { getPlaylists } from '../../utils/spotify';
+import { getPlaylists } from '../utils/spotify';
 
 interface Playlist {
   id: string;
